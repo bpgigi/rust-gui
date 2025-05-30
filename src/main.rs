738042ab6308a32fab3696d65f56c71f17203126
bpgigi <3046738904@qq.com>
2025-05-30 16:49:03 +0800

    功能: 将节点数据改为String并调整图结构

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,53 @@
 use eframe::{run_native, App, CreationContext, NativeOptions};
 use egui::Context;
-use egui_graphs::{DefaultGraphView, Graph};
-use petgraph::stable_graph::StableGraph;
+use egui_graphs::{Graph, GraphView, DefaultNodeShape, DefaultEdgeShape};
+use petgraph::stable_graph::{StableGraph, DefaultIx};
+use petgraph::Directed;
 
 pub struct BasicApp {
-    g: Graph<(), ()>,
+    g: Graph<String, ()>,
 }
 
 impl BasicApp {
     fn new(_: &CreationContext<'_>) -> Self {
-        let g = generate_graph();
-        Self { g: Graph::from(&g) }
+        let graph_data = generate_graph();
+        Self {
+            g: Graph::from(&graph_data),
+        }
     }
 }
 
 impl App for BasicApp {
     fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(&mut DefaultGraphView::new(&mut self.g));
+            ui.add(&mut GraphView::<String, (), Directed, DefaultIx, DefaultNodeShape, DefaultEdgeShape>::new(&mut self.g));
         });
     }
 }
 
-fn generate_graph() -> StableGraph<(), ()> {
-    let mut g = StableGraph::new();
+fn generate_graph() -> StableGraph<String, ()> {
+    let mut g = StableGraph::<String, ()>::new();
 
-    let a = g.add_node(());
-    let b = g.add_node(());
-    let c = g.add_node(());
+    let a = g.add_node("节点A".to_string());
+    let b = g.add_node("节点B".to_string());
+    let c = g.add_node("节点C".to_string());
+    let d = g.add_node("节点D".to_string());
+    let e = g.add_node("节点E".to_string());
 
     g.add_edge(a, b, ());
     g.add_edge(b, c, ());
     g.add_edge(c, a, ());
+    g.add_edge(a, d, ());
+    g.add_edge(d, e, ());
+    g.add_edge(e, a, ());
+    g.add_edge(b, e, ());
 
     g
 }
 
 fn main() {
     run_native(
-        "egui_graphs_basic_demo",
+        "egui_graphs_basic_standalone_demo",
         NativeOptions::default(),
         Box::new(|cc| Ok(Box::new(BasicApp::new(cc)))),
     )
